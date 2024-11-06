8b90fffcade388e22d409f11dc1e552f92870b7c
DonatoRM <donatoramos@hotmail.com>
2024-11-06 13:59:35 +0100

    feat: adapter design pattern 3

--- src/main/java/adapter/Main.java
+++ src/main/java/adapter/Main.java
@@ -4,19 +4,11 @@ import adapter.adapters.CarAdapter;
 import adapter.api.Drivable;
 import adapter.model.Car;
 import adapter.model.Cart;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main() {
-        List<Drivable> drives = new ArrayList<>();
-        Drivable cart=new CarAdapter(new Cart());
-        Drivable car=new Car();
-        drives.add(cart);
-        drives.add(car);
-        for (Drivable d : drives) {
-            d.accelerate();
-        }
+        List<Drivable> drives = List.of(new CarAdapter(new Cart()),new Car());
+        drives.forEach(Drivable::accelerate);
     }
 }
